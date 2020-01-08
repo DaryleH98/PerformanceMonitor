24ab000c62ff4ac25288874a2562738ad40845f9
D4ryl3ne1 <Darylehen02@gmail.com>
2020-01-08 15:13:50 -0500

    Added moongoose dependency

--- server/models/Machine.js
+++ server/models/Machine.js
@@ -3,6 +3,16 @@ const Schema = mongoose.Schema
 const Machine = new Schema({
   macAddress: String,
   cpuLoad: Number,
+  freeMem: Number,
+  totalMem: Number,
+  usedMem: Number,
+  memUseage: Number,
+  osType: String,
+  upTime: Number,
+  cpuModel: String,
+  numCores: Number,
+  cpuSpeed: Number,
+  cpuLoad: Number,
 })
 
 module.exports = mongoose.model('Machine', Machine)
