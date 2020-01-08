5cce2bfedba447a1231d5431708853e61f5561fe
D4ryl3ne1 <Darylehen02@gmail.com>
2020-01-08 16:10:08 -0500

    added function to determine if a new machine connected to server

--- server/socketMain.js
+++ server/socketMain.js
@@ -24,13 +24,39 @@ socket.on('clientAuth', (key)=>{
     }
 })
 
-socket.on('initPerfData', (data)=>{
+socket.on('initPerfData',  async (data)=>{
      macAddress = data.macAddress
+     const mongooseResponse = await checkAndAdd(data)
+     console.log(mongooseResponse)
 })
 
 socket.on('perfData', (data)=>{
     console.log(data)
  })
+
+}
+
+function checkAndAdd(data){
+    return new Promise((resolve, reject)=>{
+        Machine.findOne(
+            {macAddress: data.macAddress},
+            (err, doc)=>{
+                if(err){
+                    throw err;
+                    reject(err)
+                }else if(doc === null){
+                    //the record is not in the db so add it
+                    let newMachine = new Machine(data)
+                    newMachine.save()
+                    resolve('added')
+                }else{
+                    //it is in the db just resolve
+                    resolve('found')
+                }
+            }
+
+        )
+    })
 
 }
 
