87b336931c853e780639f79abba9fe850affa2fc
D4ryl3ne1 <Darylehen02@gmail.com>
2020-01-08 17:45:58 -0500

    Created widget canvass to display CPU

--- reactclient/src/Widget.js
+++ reactclient/src/Widget.js
@@ -3,17 +3,28 @@ import React, {Component} from 'react';
 import Cpu from './Cpu'
 import Mem from './Mem'
 import Info from './Info'
+import './widget.css'
 
 class Widget extends Component{
     constructor(){
-        super()
-        this.state = {}
+        super();
+        this.state = {};
     }
 
     render(){
-        const { freeMem,totalMem,usedMem,memUseage,osType,upTime,cpuModel,numCores,cpuSpeed,cpuLoad, macAddress} = this.props.data
+        const freeMem = this.props.data
+        const totalMem = this.props.data
+        const usedMem = this.props.data
+        const memUseage = this.props.data
+        const osType = this.props.data
+        const upTime = this.props.data
+        const cpuModel = this.props.data
+        const numCores = this.props.data
+        const cpuSpeed = this.props.data
+        const cpuLoad = this.props.data
+        const macAddress = this.props.data
         const cpu = {cpuLoad}
-        const mem = {totalMem, usedMem, memUseage}
+        const mem = {totalMem, usedMem, memUseage, freeMem}
         const info = {macAddress, osType, upTime, cpuModel, numCores, cpuSpeed}
         return(
             <div>
